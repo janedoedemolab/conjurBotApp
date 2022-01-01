787a58112341045528974a62245a0a98a4949625
janedoedemolab <88815482+janedoedemolab@users.noreply.github.com>
2022-01-01 18:32:07 +0800

    Update program.sh

--- program.sh
+++ program.sh
@@ -5,7 +5,7 @@ main() {
 do
   CONT_SESSION_TOKEN=$(cat /tmp/conjur_token| base64 | tr -d '\r\n')
   VAR_VALUE=$(curl -s -k -H "Content-Type: application/json" -H "Authorization: Token token=\"$CONT_SESSION_TOKEN\"" https://rhel81.alpha.local/secrets/myorg/variable/newApps%2FdbPassword-a)
-  echo "The retrieved value is: $VAR_VALUE"
+  echo "Shhh! Please keep this secret $VAR_VALUE to yourself"  
   sleep 1
 done
 }
